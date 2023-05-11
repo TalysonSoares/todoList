210e545a304bc765ab48a3b113d4501623dbedfb
TalysonSoares <talysonsoares40@gmail.com>
2023-05-11 16:01:25 -0300

    Exibindo tarefas e categorias das tarefas na tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
 import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div>
-      Olá React! 
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ])
+
+  return <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}</p>
+            <p className="category">({todo.category})</p>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  </div>
+    
 }
 
 export default App
